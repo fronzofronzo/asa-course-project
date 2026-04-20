dbf84f7c97113cf6f3e5ecd32c2fcbf3f7c461c4
Gabriele Fronzoni <gabriele.fronzoni03@gmail.com>
2026-04-20 13:51:30 +0200

    Defined agent constructor and initialized map information on the belief set

--- src/bdi/agent.js
+++ src/bdi/agent.js
@@ -1,1 +1,15 @@
-import 'dotenv'
+import 'dotenv/config'
+import { DjsConnect } from "@unitn-asa/deliveroo-js-sdk/client";
+
+const socket = new DjsConnect()
+
+class Agent {
+    constructor() {
+        this.beliefs = new BeliefSet();
+        this.id = null;
+        this.name = null;
+        this.score = 0;
+        this.x = null;
+        this.y = null;
+    }
+}
--- src/bdi/belief.js
+++ src/bdi/belief.js
@@ -1,7 +1,37 @@
-import { DjsConnect } from "@unitn-asa/deliveroo-js-sdk/client";
-
 class BeliefSet {
     constructor() {
+        this.map = {
+            width: null,
+            height: null,
+            tiles: new Map(),      // key: `${x},${y}` → { x, y, type }
+            deliveryTiles: [],     // IOTile[] where type === '2'
+            spawnTiles: [],        // IOTile[] where type === '1'
+            walkable: new Set(),   // Set of `${x},${y}` strings
+        };
+    }
 
+    /**
+     * @param {number} width
+     * @param {number} height
+     * @param {import('@unitn-asa/deliveroo-js-sdk/src/types/IOTile').IOTile[]} tiles
+     */
+    updateMap(width, height, tiles) {
+        this.map.width = width;
+        this.map.height = height;
+        this.map.tiles.clear();
+        this.map.deliveryTiles = [];
+        this.map.spawnTiles = [];
+        this.map.walkable.clear();
+
+        for (const tile of tiles) {
+            const key = `${tile.x},${tile.y}`;
+            this.map.tiles.set(key, tile);
+
+            if (tile.type !== '0') this.map.walkable.add(key);
+            if (tile.type === '2') this.map.deliveryTiles.push(tile);
+            if (tile.type === '1') this.map.spawnTiles.push(tile);
+        }
     }
 }
+
+export { BeliefSet };
